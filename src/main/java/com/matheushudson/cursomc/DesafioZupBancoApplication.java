01f7439baf7aa2c4d2547f606d0c508ba1e7d83b
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2021-01-01 10:57:50 -0300

    Projeto criado

---
+++ src/main/java/com/matheushudson/cursomc/DesafioZupBancoApplication.java
@@ -0,0 +1,13 @@
+package com.matheushudson.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZupBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioZupBancoApplication.class, args);
+	}
+
+}
